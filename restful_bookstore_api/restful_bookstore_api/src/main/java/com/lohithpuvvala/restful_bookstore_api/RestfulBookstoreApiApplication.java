c7aeef4f016a965b7cc08f08a67f39e80d673a7c
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-08 23:10:37 +0530

    Initialize Project 1: Restful_Bookstore_API

---
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/RestfulBookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.lohithpuvvala.restful_bookstore_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulBookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulBookstoreApiApplication.class, args);
+	}
+
+}
---
+++ restful_bookstore_api/restful_bookstore_api/src/test/java/com/lohithpuvvala/restful_bookstore_api/RestfulBookstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lohithpuvvala.restful_bookstore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulBookstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
