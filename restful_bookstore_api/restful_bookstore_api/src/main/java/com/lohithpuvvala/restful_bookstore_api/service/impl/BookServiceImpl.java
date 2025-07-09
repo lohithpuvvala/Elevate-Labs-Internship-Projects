9d5311404582a83e98f87f45a18cbc0b42f8c2f0
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-09 09:55:49 +0530

    Create BookServiceImpl Exoskeleton in Service.Impl(Implementation) Layer

---
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/impl/BookServiceImpl.java
@@ -0,0 +1,33 @@
+package com.lohithpuvvala.restful_bookstore_api.service.impl;
+
+import com.lohithpuvvala.restful_bookstore_api.model.Book;
+import com.lohithpuvvala.restful_bookstore_api.service.BookService;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    @Override
+    public Book createBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book getBookById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteBookById(long id) {
+
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return List.of();
+    }
+}
