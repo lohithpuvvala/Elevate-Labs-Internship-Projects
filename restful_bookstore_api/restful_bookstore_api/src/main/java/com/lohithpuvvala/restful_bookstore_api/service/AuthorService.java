b125e8984c42736e9acf61710153939375e73480
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-09 10:22:33 +0530

    Update AuthorSerive Interface to allow DTO objects. Update getAllAuthors in AuthorServiceImpl

--- restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/AuthorService.java
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/AuthorService.java
@@ -1,14 +1,16 @@
 package com.lohithpuvvala.restful_bookstore_api.service;
 
+import com.lohithpuvvala.restful_bookstore_api.dto.AuthorDetailDto;
+import com.lohithpuvvala.restful_bookstore_api.dto.AuthorDto;
 import com.lohithpuvvala.restful_bookstore_api.model.Author;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
 public interface AuthorService {
-    Author createAuthor(Author author);
-    Author getAuthorById(long id);
-    List<Author> getAllAuthors();
-    Author updateAuthor(Author author);
+    AuthorDto createAuthor(Author author);
+    AuthorDetailDto getAuthorById(long id);
+    List<AuthorDto> getAllAuthors();
+    AuthorDto updateAuthor(Author author);
     void deleteAuthorById(long id);
 }
--- restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/impl/AuthorServiceImpl.java
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/impl/AuthorServiceImpl.java
@@ -1,28 +1,47 @@
 package com.lohithpuvvala.restful_bookstore_api.service.impl;
 
+import com.lohithpuvvala.restful_bookstore_api.dto.AuthorDetailDto;
+import com.lohithpuvvala.restful_bookstore_api.dto.AuthorDto;
+import com.lohithpuvvala.restful_bookstore_api.mapper.DtoMapper;
 import com.lohithpuvvala.restful_bookstore_api.model.Author;
+import com.lohithpuvvala.restful_bookstore_api.repository.AuthorRepository;
 import com.lohithpuvvala.restful_bookstore_api.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
+
+    @Autowired
+    private final AuthorRepository authorRepository;
+
     @Override
-    public Author createAuthor(Author author) {
+    public List<AuthorDto> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+        return authors.stream()
+                .map(DtoMapper::toAuthorDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AuthorDto createAuthor(Author author) {
+
+    }
+
+    @Override
+    public AuthorDetailDto getAuthorById(long id) {
         return null;
     }
 
-    @Override
-    public Author getAuthorById(long id) {
-        return null;
-    }
 
     @Override
-    public List<Author> getAllAuthors() {
-        return List.of();
-    }
-
-    @Override
-    public Author updateAuthor(Author author) {
+    public AuthorDto updateAuthor(Author author) {
         return null;
     }
 
