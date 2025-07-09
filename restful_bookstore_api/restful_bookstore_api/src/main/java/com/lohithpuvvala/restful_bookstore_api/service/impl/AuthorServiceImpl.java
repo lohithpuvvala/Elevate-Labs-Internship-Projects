e9a5e0f04618f102cfe009d8239446b2eab29b48
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-09 09:54:21 +0530

    Create AuthorServiceImpl in Service.Implementation Layer.

---
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,33 @@
+package com.lohithpuvvala.restful_bookstore_api.service.impl;
+
+import com.lohithpuvvala.restful_bookstore_api.model.Author;
+import com.lohithpuvvala.restful_bookstore_api.service.AuthorService;
+
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService {
+    @Override
+    public Author createAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author getAuthorById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        return List.of();
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(long id) {
+
+    }
+}
