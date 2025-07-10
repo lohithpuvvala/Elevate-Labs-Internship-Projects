ae3a8a38d9515edcd6d7ae8e02cae8487e7235d8
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-10 12:26:15 +0530

    Add Name and Email Validation to CreateAuthorDto

--- restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/dto/CreateAuthorDto.java
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/dto/CreateAuthorDto.java
@@ -1,9 +1,15 @@
 package com.lohithpuvvala.restful_bookstore_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateAuthorDto {
+    @NotBlank(message = "Author name is requred!")
     private String name;
+
+    @NotBlank(message = "Email is requred!")
+    @Email(message = "Invalid Email Format!")
     private String email;
 }
