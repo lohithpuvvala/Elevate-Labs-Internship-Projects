935697bc7f3e692a965603efada75fd4619aa3bf
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-10 11:10:28 +0530

    Created BookController in Controller Layer. Added Endpoints for CURD Operations

---
+++ restful_bookstore_api/restful_bookstore_api/src/main/java/com/lohithpuvvala/restful_bookstore_api/controller/BookController.java
@@ -0,0 +1,54 @@
+package com.lohithpuvvala.restful_bookstore_api.controller;
+
+import com.lohithpuvvala.restful_bookstore_api.dto.BookDetailDto;
+import com.lohithpuvvala.restful_bookstore_api.dto.BookDto;
+import com.lohithpuvvala.restful_bookstore_api.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDetailDto> createBook(@RequestBody BookDto bookDto){
+        BookDetailDto created = bookService.createBook(bookDto);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDetailDto>> getAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDetailDto> getBookById(@PathVariable Long id){
+        BookDetailDto bookDetailDto = bookService.getBookById(id);
+        return ResponseEntity.ok(bookDetailDto);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDetailDto> updateBook(@PathVariable Long id, @RequestBody BookDto bookDto){
+        BookDetailDto updated = bookService.updateBook(id,bookDto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable Long id){
+        bookService.deleteBookById(id);
+        return ResponseEntity
+                .ok("Book with id :: "+id+" deleted successfully");
+    }
+}
