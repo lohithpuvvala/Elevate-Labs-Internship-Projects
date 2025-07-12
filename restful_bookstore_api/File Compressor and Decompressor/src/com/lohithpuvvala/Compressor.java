4a4bacc88ac17883f42b9e23cf44a65837d1c7ae
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-12 12:44:31 +0530

    Updated Compressor.java with Progress Bar Support

--- restful_bookstore_api/File Compressor and Decompressor/src/com/lohithpuvvala/Compressor.java
+++ restful_bookstore_api/File Compressor and Decompressor/src/com/lohithpuvvala/Compressor.java
@@ -1,16 +1,29 @@
 package com.lohithpuvvala;
 
+import javax.swing.*;
 import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Compressor {
-    public static void compressFiles(File[] inputFiles, File outputZip) throws IOException {
+
+    //Existing Method (no progress bar)
+    public static void compressFiles(File[] inputFile, File outputZip) throws IOException {
+        compressFiles(inputFile, outputZip, null);
+    }
+
+    //Overloaded Method with progress bar
+    public static void compressFiles(File[] inputFiles, File outputZip, JProgressBar progressBar) throws IOException {
         try(
             FileOutputStream fos = new FileOutputStream(outputZip);
             ZipOutputStream zos = new ZipOutputStream(fos);
         ){
+            int totalFiles = inputFiles.length;
+            int currentFile = 0;
+
             for(File file: inputFiles){
+                currentFile++;
+
                 FileInputStream fis = new FileInputStream(file);
                 ZipEntry zipEntry = new ZipEntry(file.getName());
                 zos.putNextEntry(zipEntry);
@@ -23,6 +36,12 @@ public class Compressor {
 
                 zos.closeEntry();
                 fis.close();
+
+                //Update Progress
+                if(progressBar != null){
+                    int progress = (int)(((double)currentFile / (double)totalFiles) * 100);
+                    SwingUtilities.invokeLater(() -> progressBar.setValue(progress));
+                }
             }
         }
     }
